54b8b0cf7fe67394cfbe07163498567f1fc3e026
EC2 Default User <ec2-user@ip-172-31-41-203.eu-west-1.compute.internal>
2020-04-25 17:00:29 +0000

    added security group

--- provider.tf
+++ provider.tf
@@ -14,3 +14,34 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+resource "aws_security_group" "allow_tls" {
+  name        = "first_terraform"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-0dca0e74a352343a4"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "first_terraform"
+  }
+}
