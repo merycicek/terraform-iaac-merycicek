460285d30db6081978a8c6f47faca56691e4d489
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-27 23:49:05 +0000

    add out key pair

---
+++ Class-2/output.tf
@@ -0,0 +1,14 @@
+output "name" {
+  value = "meryem"
+}
+output "lastname" {
+  value = "Elibal"
+}
+resource "aws_key_pair" "ec2-ireland2" {
+  key_name   = "meryems_terra_key2"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+output "key_name" {
+  value = "${aws_key_pair.ec2-ireland2.key_name}"
+}
