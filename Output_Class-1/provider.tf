523e3908fa57e6da104ac816143a244c8f1dbb1d
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-28 15:14:55 +0000

    added keypairs and out puts

--- Output_Class-1/provider.tf
+++ Output_Class-1/provider.tf
@@ -3,5 +3,29 @@ provider "aws" {
   version = "2.59"
 }
 
+// Creates Key
+resource "aws_key_pair" "ec2-ireland2" {
+  key_name   = "meryems_terra_key2"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+
+  
+}
+
+
+
+// Creates Instance
+
+
+// Creates sec group
+
+
+// Creates Route53
+
+
+// Outputs everythings
+
+output "key_pair" {
+  value = "${aws_key_pair.ec2-ireland2.key_name}"
+}
 
 
