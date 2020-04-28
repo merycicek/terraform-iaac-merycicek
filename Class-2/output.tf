9a11ed8048fc910c21f200274c569464edc4cdc2
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-28 14:44:14 +0000

    added vpc and vpc output

--- Class-2/output.tf
+++ Class-2/output.tf
@@ -5,6 +5,14 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  
+}
+output "aws_vpc_id" {
+  value = "${aws_vpc.main.id}"
+}
+
 
 output "instance_id"{
    value = "${aws_instance.web.id}"
