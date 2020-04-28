5a9bcc989add81d7bf182a90a87c0f3ed6373091
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-28 17:03:56 +0000

    added output taskfiles

---
+++ Task_output/output.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "eu-west-1"
+  version = "2.59"
+}
+
+// Creates Key
+resource "aws_key_pair" "ec2-ireland3" {
+  key_name   = "meryems_terra_key3"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+
+  
+}
+// Creates Instance
+resource "aws_instance" "web-output" {
+  ami           = "ami-06ce3edf0cff21f07"
+  instance_type = "t2.micro"
+}
+// Creates sec group
+
+resource "aws_security_group" "allow_tls" { 
+name = "terraform_task" 
+  
+}
+resource "aws_route53_record" "web"{
+     zone_id = "${aws_route53_zone.web.zone_id}"
+}
+
+
+// Outputs everythings
+
+output "key_pair" {
+  value = "${aws_key_pair.ec2-ireland3.key_name}"
+}
+
+output "instance_id" {
+  value = "${aws_instance.web-output.id}"
+}
+ output "instance_arn"{
+   value = "${aws_instance.web-output.arn}"
+ }
+
+ output "instance_ip" {
+   value = "${aws_instance.web-output.public_ip}"
+ }
+ output "my_message" {
+   value = " login to wordpress and reset passwd "
+ }
+
+
---
+++ Task_output/provider.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  region = "eu-west-1"
+  version = "2.59"
+}
+terraform {  
+  required_version = "0.11.14"
+}
