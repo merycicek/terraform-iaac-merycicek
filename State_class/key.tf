d666ee93fa67afe9d0286d8577b329ec96a72460
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-30 00:15:32 +0000

    ...

--- State_class/key.tf
+++ State_class/key.tf
@@ -5,7 +5,7 @@ resource "aws_key_pair" "us-east-1-key" {
   
 }
 resource "aws_instance" "web" {
-    instan_type = "t2.micro"
+    instance_type = "t2.micro"
     ami = "i-00d48151d54550b1a"
   # (resource arguments)
 }
